ff9b62179c8b31423fd12a31fd2c5ef9fbc40884
João Lima <joaovictorbernardinodelima@gmail.com>
2025-12-11 11:16:21 -0300

    add exercicios

---
+++ index.js
@@ -0,0 +1,8 @@
+const nome = "João Lima";
+const idade = 29;
+const profissao = "desenvolvedor de software";
+const msg = `${nome} tem ${idade} anos de idade e trabalha como ${profissao}`;
+
+console.log(msg);
+
+
